7102d186b05582ae5482a3dedeb810b06ea1957b
imhotepper <stefanescu.dragos@gmail.com>
2018-11-21 18:45:45 +0200

    initial commit

---
+++ Worker/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace Worker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Worker executed @: " + DateTime.Now.ToString("yyyy-MM-dd hh:mm:sss"));
+                Thread.Sleep((1000));
+            }
+            
+        }
+    }
+}
